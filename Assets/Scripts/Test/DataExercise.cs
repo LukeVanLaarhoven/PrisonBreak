6f8d874b8e23c26cf40c06b77fed5cfe8c95ca6b
LukeVanLaarhoven <60391960+LukeVanLaarhoven@users.noreply.github.com>
2021-03-25 18:27:02 +0100

    yo

---
+++ Assets/Scripts/Test/DataExercise.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataExercise : MonoBehaviour
+{
+    List<string> words;
+
+    private void Start()
+    {
+        words = new List<string>();
+        
+        
+    }
+}
