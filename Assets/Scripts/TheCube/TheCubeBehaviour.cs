218f15ee3ce28cefc7128968e2c5f2d85c59d286
LukeVanLaarhoven <60391960+LukeVanLaarhoven@users.noreply.github.com>
2021-03-30 15:06:07 +0200

    Cube moves

---
+++ Assets/Scripts/TheCube/TheCubeBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheCubeBehaviour : MonoBehaviour
+{
+    public GameObject[] searchLights;
+    public GameObject[] nodes;
+
+    private float ranNum;
+    private int ranNode;
+    public float movementSpeed;
+    private float timer;
+
+    private void Start()
+    {
+        ranNum = Random.Range(-7, 7);
+
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        searchLights[0].transform.Rotate(Vector3.up * Time.deltaTime * ranNum, Space.World);
+        searchLights[1].transform.Rotate(Vector3.up * Time.deltaTime * ranNum, Space.World);
+        searchLights[2].transform.Rotate(Vector3.up * Time.deltaTime * ranNum, Space.World);
+        searchLights[3].transform.Rotate(Vector3.up * Time.deltaTime * ranNum, Space.World);
+
+        gameObject.transform.position = Vector3.MoveTowards(transform.position, nodes[ranNode].transform.position, movementSpeed);
+
+        if (timer < 0)
+        {
+            ranNode = Random.Range(0, 4);
+
+            timer = 2;
+        }
+    }
+}
